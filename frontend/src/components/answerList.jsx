1674d6bba27e0d09fb4bc64a2a870aebb8901189
Olatundeawo <awotimilehin.babatunde@gmail.com>
2025-07-31 18:21:13 +0100

    created a component that's link by id and the display the related answer for edit and delete

---
+++ frontend/src/components/answerList.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function AnswerList() {
+  const { id } = useParams();
+
+  return <div>See the page</div>;
+}
